09089f02607d51a16b9ed58da4d1bee74d7bd0cd
行君 <ruihang@foxmail.com>
2017-12-11 21:38:28 +0800

    update commit

--- src/main/java/top/ruix/scdemo2/UserController.java
+++ src/main/java/top/ruix/scdemo2/UserController.java
@@ -1,9 +1,7 @@
 package top.ruix.scdemo2;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Ruixian
@@ -18,6 +16,20 @@ public class UserController {
     @GetMapping("/{id}")
     public User findById(@PathVariable Long id) {
         User findOne = this.userRepository.findOne(id);
+        return findOne;
+    }
+
+    @GetMapping("/get")
+    public User get(Long id, String username) {
+        System.out.println("id=" + id + ", username=" + username);
+        User findOne = this.userRepository.findOne(id);
+        return findOne;
+    }
+
+    @PostMapping("/post")
+    public User post(@RequestBody User user) {
+        System.out.println("id=" + user.getId() + ", username=" + user.getUsername());
+        User findOne = this.userRepository.findOne(user.getId());
         return findOne;
     }
 
